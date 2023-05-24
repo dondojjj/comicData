d3627a6aa9a973f98888fb4c068971ccce63373e
Juan Arredondo <jfarredondom@gmail.com>
2023-05-24 11:27:56 -0400

    added date restriction extraction

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def slugify(value, allow_unicode=False):
 
 def logica_uri(uri):
     image_url = "https://www.comics.org{}".format(uri)
+    print("url: {}".format(image_url))
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'}
     # used proxies because of connection restrictions (can be removed)
     proxies = {
@@ -37,16 +38,18 @@ def logica_uri(uri):
     # setting image name for download. desired name example: HILLMAN_OCT1952_crime-must-stop-comics_v1-1
     soup = BeautifulSoup(response.content, 'html.parser')
     series = soup.find("div", class_="item_data flex_left").a.get_text(strip=True)
-    print("serie: {}".format(series))
+    # print("serie: {}".format(series))
     name = soup.find("div", class_="left").span.a.get_text(strip=True)
-    print("name: {}".format(name))
+    # print("name: {}".format(name))
     try:
         issue_number = soup.find("span", class_="issue_number").text
     except AttributeError:
         issue_number = ''
-    print("issue_number: {}".format(issue_number))
+    # print("issue_number: {}".format(issue_number))
     date = soup.find("div", class_="right").get_text(strip=True)
-    print("date: {}".format(date))
+    if len(date) > 30:
+        date = ''
+    # print("date: {}".format(date))
     image_name = "{}_{}_{}_{}".format(series,date,name,issue_number)
     # normalize string
     image_name = slugify(image_name)
@@ -63,8 +66,8 @@ def logica_uri(uri):
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # 817 - pages
 # number of pages are known
-for page in [26,27]: #range(817):
-    print("Connecting to URL: ")
+for page in range(26,817): #range(817):
+
     comics_covers = "https://www.comics.org/search/advanced/process/?target=issue_cover&method=icontains&logic=False&type=19&order1=series&order2=date&start_date=01%2F01%2F1900&end_date=01%2F01%2F1961&country=us&page={}".format(
         page + 1)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'}
@@ -73,6 +76,7 @@ for page in [26,27]: #range(817):
         'http': 'http://127.0.0.1:8080',
         'https': 'http://127.0.0.1:8080'
     }
+    print("Connecting to URL: {}".format(comics_covers))
     # Send a GET request to the image URL
     response = requests.get(comics_covers, headers=headers, proxies=proxies, verify=False)
     print("downloading images page {}: ".format(page + 1))
